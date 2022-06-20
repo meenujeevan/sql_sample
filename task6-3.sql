c4b2e15b22b0c93cf7e2b31f86d3872db9fbbd97
meenujeevan <92151400+meenujeevan@users.noreply.github.com>
2022-06-20 15:43:01 +0530

    Delete task6-3.sql

--- task6-3.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM worker.employee WHERE Empid IN(  
-       SELECT Empid FROM (SELECT Empid, ROW_NUMBER()   
-       OVER (PARTITION BY Email ORDER BY Email) AS row_num   
-       FROM worker.employee) AS temp_table WHERE row_num>1);  
